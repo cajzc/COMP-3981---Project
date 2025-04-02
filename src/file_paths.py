3741247211df6337e1e133f6f858d3b2974b8ff9
cajzc <cainan@macpost.com>
2025-04-01 17:47:00 -0700

    Add functions for reading to and from game output files

--- src/file_paths.py
+++ src/file_paths.py
@@ -3,6 +3,7 @@ import os
 import sys
 from enum import Enum
 
+
 class FilePaths(Enum):
     """Represents the file paths for the running program."""
     PROJECT_ROOT = os.path.dirname(os.path.abspath(sys.executable)) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -16,4 +17,28 @@ class FilePaths(Enum):
     MOVES = os.path.join(GAME_OUTPUT, "moves.txt")
     BOARD_INPUT = os.path.join(GAME_OUTPUT, "board_input.txt")
     BOARD_OUTPUT = os.path.join(GAME_OUTPUT, "board_output.txt")
+
+
+def write_to_output_game_file(file_path: FilePaths, data: str):
+    """
+    Writes the game state to an output .txt file. Creates the file if it doesn't exist. Writes only to the first line
+    in the file. Overwrites any data.
+
+    This might be used to write the ai's generated move, or current board state in the format C5b, ...
+    """
+    with open(file_path.value, "w") as file:
+        file.write(data)
+
+
+def read_from_output_game_file(file_path: FilePaths) -> str:
+    """
+    Reads a game state from an output .txt file. Reads the single first line.
+
+    This might be used to read the output board configuration from the GUI, in the format C5b, ...
+    """
+    try:
+        with open(file_path.value, "r") as file:
+            return file.readline().strip()
+    except FileNotFoundError:
+        return ""
 
