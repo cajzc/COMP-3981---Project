5cf9c512f42411593ba5d2d8cfbeceb3ad6d6a43
cajzc <cainan@macpost.com>
2025-04-02 17:09:23 -0700

    Add debug for checking if file exists

--- src/file_paths.py
+++ src/file_paths.py
@@ -53,21 +53,22 @@ def read_from_output_game_file(file_path: FilePaths, last_modified_time) -> Tupl
     :param last_modified_time: the last time the file has been read from
     :returns: the data in the file as a str and the timestamp from which it was modified
     """
-    # Wait for the file to be created
+    # Wait for the file to exist
     while not os.path.exists(file_path.value):
-        print(f"File {file_path.value} not found.")
+        print(f"File {file_path.value} not found. Waiting...")
         time.sleep(0.1)
 
-    # Wait for the file to be modified
     while True:
         current_modified_time = os.path.getmtime(file_path.value)
-        print(f"Waiting for modifications to {file_path.value}.")
-
+        
         if current_modified_time != last_modified_time:
             last_modified_time = current_modified_time
 
             with open(file_path.value, "r", encoding="utf-8") as file:
-                return (file.readline().strip(), last_modified_time)
+                board_str = file.readline().strip()
+
+            print(f"File modified! New board state: {board_str}")
+            
+            return board_str, last_modified_time  # Reads, updates, and keeps running
 
         time.sleep(0.1)
-
